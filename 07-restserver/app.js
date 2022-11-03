18aa64e4992192484eb2d449d74ea214e7499426
chazarreta <cintia.chazarreta@accenture.com>
2022-11-03 12:51:41 -0300

    seccion 8

---
+++ 07-restserver/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto',process.env.PORT )
+
+});
